36183ae99e1f85b111818c142077f360721e456d
Pengcheng Li <lpc1983@gmail.com>
2014-11-23 01:05:01 +0800

    Add root gravity parameter

--- cocos2d-x-lua/cocos2dx.lua
+++ cocos2d-x-lua/cocos2dx.lua
@@ -1,31 +1,34 @@
 -- Generate by Physics Editor Don't edit it --
 
-return {{% for body in bodies %}
-    {{body.name}} = {  
-        dynamic = {% if body.dynamic %}true{% else %}false{% endif %},
-        enable = {% if body.enable %}true{% else %}false{% endif %},
-        fixtures = {{% for fixture in body.fixtures %}
-            {
-                density = {{fixture.density|floatformat}},
-                restitution = {{fixture.restitution|floatformat}},
-                friction = {{fixture.friction|floatformat}},
-                group = {{fixture.group}},   
-                categoryBitmask = {{fixture.categoryBitmask}},
-                collisionBitmask = {{fixture.collisionBitmask}},
-                contactTestBitmask = {{fixture.contactTestBitmask}},{% if fixture.isCircle %}
-                isCircle = true,
-                radius = {{fixture.radius|floatformat}},
-                offset = {
-                    x = {{fixture.center.x|floatformat}},
-                    y = {{fixture.center.y|floatformat}}
-                },{% else %}
-                isCircle = false,
-                polygonsVerts = {{% for polygon in fixture.polygons %}
-                    {{% for point in polygon %}
-                        cc.p({{point.x|floatformat}}, {{point.y|floatformat}}){% if not forloop.last %},{% endif %}{% endfor %}
-                    }{% if not forloop.last %},{% endif %}{% endfor %}
-                }{% endif %}
-            }{% if not forloop.last %},{% endif %}{% endfor %}
-        }
-    }{% if not forloop.last %},{% endif %}{% endfor %}
+return {
+    gravity = {{gravity|floatformat}},
+    bodys = {{% for body in bodies %}
+        {{body.name}} = {  
+            dynamic = {% if body.dynamic %}true{% else %}false{% endif %},
+            enable = {% if body.enable %}true{% else %}false{% endif %},
+            fixtures = {{% for fixture in body.fixtures %}
+                {
+                    density = {{fixture.density|floatformat}},
+                    restitution = {{fixture.restitution|floatformat}},
+                    friction = {{fixture.friction|floatformat}},
+                    group = {{fixture.group}},   
+                    categoryBitmask = {{fixture.categoryBitmask}},
+                    collisionBitmask = {{fixture.collisionBitmask}},
+                    contactTestBitmask = {{fixture.contactTestBitmask}},{% if fixture.isCircle %}
+                    isCircle = true,
+                    radius = {{fixture.radius|floatformat}},
+                    offset = {
+                        x = {{fixture.center.x|floatformat}},
+                        y = {{fixture.center.y|floatformat}}
+                    },{% else %}
+                    isCircle = false,
+                    polygonsVerts = {{% for polygon in fixture.polygons %}
+                        {{% for point in polygon %}
+                            cc.p({{point.x|floatformat}}, {{point.y|floatformat}}){% if not forloop.last %},{% endif %}{% endfor %}
+                        }{% if not forloop.last %},{% endif %}{% endfor %}
+                    }{% endif %}
+                }{% if not forloop.last %},{% endif %}{% endfor %}
+            }
+        }{% if not forloop.last %},{% endif %}{% endfor %}
+    }
 }
