a2d8bede46ad09aa1076653493a719b3a34d480d
Pengcheng Li <lpc1983@gmail.com>
2014-11-22 11:26:19 +0800

    Formate cocos2dx.lua and change some default value

--- cocos2d-x-lua/cocos2dx.lua
+++ cocos2d-x-lua/cocos2dx.lua
@@ -1,31 +1,31 @@
 -- Generate by Physics Editor Don't edit it --
 
-return { {% for body in bodies %}
+return {{% for body in bodies %}
     {{body.name}} = {  
         dynamic = {% if body.dynamic %}true{% else %}false{% endif %},
         enable = {% if body.enable %}true{% else %}false{% endif %},
-        fixtures = { {% for fixture in body.fixtures %}
+        fixtures = {{% for fixture in body.fixtures %}
             {
-                density = {{fixture.density}},
-                restitution = {{fixture.restitution}},
-                friction = {{fixture.friction}},
+                density = {{fixture.density|floatformat}},
+                restitution = {{fixture.restitution|floatformat}},
+                friction = {{fixture.friction|floatformat}},
                 group = {{fixture.group}},   
                 categoryBitmask = {{fixture.categoryBitmask}},
                 collisionBitmask = {{fixture.collisionBitmask}},
                 contactTestBitmask = {{fixture.contactTestBitmask}},{% if fixture.isCircle %}
                 isCircle = true,
-                radius = {{fixture.radius}},
+                radius = {{fixture.radius|floatformat}},
                 offset = {
-                    x = {{fixture.center.x}},
-                    y = {{fixture.center.y}}
+                    x = {{fixture.center.x|floatformat}},
+                    y = {{fixture.center.y|floatformat}}
                 },{% else %}
                 isCircle = false,
-                polygonsVerts = { {% for polygon in fixture.polygons %}
+                polygonsVerts = {{% for polygon in fixture.polygons %}
                     {{% for point in polygon %}
-                        cc.p({{point.x}}, {{point.y}}){% if not forloop.last %},{% endif %}{% endfor %}
+                        cc.p({{point.x|floatformat}}, {{point.y|floatformat}}){% if not forloop.last %},{% endif %}{% endfor %}
                     }{% if not forloop.last %},{% endif %}{% endfor %}
                 }{% endif %}
             }{% if not forloop.last %},{% endif %}{% endfor %}
+        }
     }{% if not forloop.last %},{% endif %}{% endfor %}
-}   
-
+}
